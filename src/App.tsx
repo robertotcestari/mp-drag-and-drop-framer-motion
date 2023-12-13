db009f99d2c69655cbd39790167c4b0d0ca48bb7
Roberto Cestari <robertotcestari@gmail.com>
2023-12-13 20:03:10 -0300

    resolution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import songs from './data/songs.json';
+import { useState } from 'react';
+import songList from './data/songs.json';
+import { Reorder } from 'framer-motion';
+
+type Song = {
+  id: number;
+  title: string;
+  artist: string;
+  duration: string;
+  image: string;
+};
+
 
 function App() {
+
+  const [songs, setSongs] = useState<Song[]>(songList);
+
+  function handleReorder(newValues: Song[]) {
+    setSongs(newValues);
+  }
+
+
   return (
     <div className="flex items-center justify-center min-h-screen text-slate-800 bg-zinc-700">
       <div className="w-full max-w-lg px-4 py-10 my-10 bg-white rounded-lg shadow-2xl">
@@ -10,16 +29,21 @@ function App() {
           </h1>
         </div>
         <div className="">
-          <div>
+          <Reorder.Group as="div"
+            axis="y"
+            values={songs}
+            onReorder={handleReorder}
+            >
             {songs.map((song) => (
-              <div
+              <Reorder.Item as="div"
+                value={song}
                 key={song.id}
                 className="flex items-center w-full gap-6 p-4 mx-auto mb-4 card "
               >
                 <span className="text-slate-400 border-slate-200 cursor-grab">
-                  <img src="/grab-icon.svg" alt="grab icon" className="w-8 h-8" />
+                  <img src="/grab-icon.svg" alt="grab icon" className="w-8 h-8 pointer-events-none" />
                 </span>
-                <img src={song.image} alt="" className="w-14 hue-rotate-30" />
+                <img src={song.image} alt="" className="pointer-events-none w-14 hue-rotate-30" />
                 <div className="w-full card-header">
                   <div className="flex items-center justify-between">
                     <h3 className="font-bold ">{song.title}</h3>
@@ -27,9 +51,9 @@ function App() {
                   </div>
                   <p className="text-sm text-slate-400">{song.artist}</p>
                 </div>
-              </div>
+              </Reorder.Item>
             ))}
-          </div>
+          </Reorder.Group>
         </div>
       </div>
     </div>
